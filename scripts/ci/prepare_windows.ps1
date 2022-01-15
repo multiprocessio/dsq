9b8baa828be2078d717320a71bd4f0a5d42f0fda
Phil Eaton <phil@eatonphil.com>
2022-01-14 21:51:58 -0500

    Zip not 7z

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -1,3 +1,3 @@
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
-scoop install go jq 7z
+scoop install go jq zip
