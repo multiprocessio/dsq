cc573d8e8ad0e570f47e059cdbed6d1360d9dbe2
Phil Eaton <phil@eatonphil.com>
2022-03-15 21:17:14 +0000

    Try not using scoop

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -1,4 +1,11 @@
 iwr -useb 'https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1' -outfile 'install.ps1'
 .\install.ps1 -RunAsAdmin
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
-scoop install go@1.18 jq zip
+scoop install jq zip curl
+
+curl -L -O "https://go.dev/dl/go1.18.windows-amd64.zip"
+unzip go1.18.windows-amd64.zip
+ls
+ls go1.18.windows-amd64
+cp go1.18.windows-amd64\go (Join-Path (Resolve-Path ~).Path "scoop\shims")\go
+cp go1.18.windows-amd64\gofmt (Join-Path (Resolve-Path ~).Path "scoop\shims")\gofmt
