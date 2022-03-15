7b2898941009462ff5ef93c7ac25ff589d2d0b99
Phil Eaton <phil@eatonphil.com>
2022-03-15 21:06:20 +0000

    Try not using brew

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -5,5 +5,5 @@ set -eux
 sudo curl -LO https://go.dev/dl/go1.18.darwin-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.18.darwin-amd64.tar.gz
-sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
-sudo ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
+sudo mv /usr/local/go/bin/go /usr/local/bin/go
+sudo mv /usr/local/go/bin/gofmt /usr/local/bin/gofmt
