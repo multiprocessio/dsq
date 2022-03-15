ecf79d617216f4051b58085966075f7005c6e4ca
Phil Eaton <phil@eatonphil.com>
2022-03-15 21:20:51 +0000

    Try a different approach

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -5,7 +5,9 @@ scoop install jq zip curl
 
 curl -L -O "https://go.dev/dl/go1.18.windows-amd64.zip"
 unzip go1.18.windows-amd64.zip
-ls
-ls go1.18.windows-amd64
-cp go1.18.windows-amd64\go (Join-Path (Resolve-Path ~).Path "scoop\shims")\go
-cp go1.18.windows-amd64\gofmt (Join-Path (Resolve-Path ~).Path "scoop\shims")\gofmt
+ls go
+ls go\bin
+Join-Path $pwd "go\bin" >> $Env:GITHUB_PATH
+which go
+where go
+go version
