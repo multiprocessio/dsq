a066be314b91b5e2d91c59b15ecc75f4da9b855a
Phil Eaton <phil@eatonphil.com>
2022-06-16 18:18:22 +0900

    Fix go install/

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -1,10 +1,10 @@
 iwr -useb 'https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1' -outfile 'install.ps1'
 .\install.ps1 -RunAsAdmin
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
-scoop install jq zip curl 7zip
+scoop install jq zip curl 7zip go@1.18.3
 
-curl -L -O "https://go.dev/dl/go1.18.windows-amd64.zip"
-unzip go1.18.windows-amd64.zip
-Join-Path $pwd "go\bin" >> $Env:GITHUB_PATH
+# curl -L -O "https://go.dev/dl/go1.18.windows-amd64.zip"
+# unzip go1.18.windows-amd64.zip
+# Join-Path $pwd "go\bin" >> $Env:GITHUB_PATH
 
 7z e testdata/taxi.csv.7z
