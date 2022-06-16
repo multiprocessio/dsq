624f6bbd330f7e0e958f33f568f0f0c2ca5f9a58
Phil Eaton <phil@eatonphil.com>
2022-06-16 18:28:20 +0900

    Try failing in ps1

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -1,7 +1,21 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
+
 iwr -useb 'https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1' -outfile 'install.ps1'
 .\install.ps1 -RunAsAdmin
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
-scoop install jq zip curl 7zip go@1.18.3
+scoop install jq
+scoop install zip
+scoop install curl
+scoop install 7zip
+scoop install go@1.18.3
 
 # curl -L -O "https://go.dev/dl/go1.18.windows-amd64.zip"
 # unzip go1.18.windows-amd64.zip
